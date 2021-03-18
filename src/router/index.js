8a0db81b3bdb8be5c4c149dc3ab9d5d260e21b61
Denny Sihol Ronaldo <57458599+dennysihol@users.noreply.github.com>
2021-03-18 20:21:07 +0700

    template for Rulse and Plays

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/rules',
+    name: 'Rules',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Rules.vue')
+  },
+  {
+    path: '/plays',
+    name: 'Plays',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Plays.vue')
   }
 ]
 
